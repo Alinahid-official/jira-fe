571bc4e2ca37521f3e9e8cda25806a1ce2e30f0a
alinahid <nahidali412@gmail.com>
2022-07-14 13:41:33 +0530

    first commit

---
+++ layout/auth.js
@@ -0,0 +1,9 @@
+export default function Auth({children}){
+    return(
+        <div>
+            <h1>Header</h1>
+            {children}
+            <footer>footer</footer>
+        </div>
+    )
+}
---
+++ pages/login.js
@@ -0,0 +1,12 @@
+import Layout from '../layout/auth'
+export default function Login(){
+    return(
+        <div>
+            <Layout>
+                <div>
+                login page
+                </div>
+            </Layout>
+        </div>
+    )
+}
