891f073359985e2cdbe38282141fe04e5884de8f
alinahid <nahidali412@gmail.com>
2022-07-15 22:14:26 +0530

    7/15/2022 nahid

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,25 @@
 import Layout from '../layout/auth'
+import React, { useState, useContext } from "react";
+import { Context } from "../context";
+
 export default function Login(){
+    const { state, dispatch } = useContext(Context);
     return(
         <div>
             <Layout>
                 <div>
-                login page
+                    <div>
+                        login page
+                    </div>
+                    <button
+                    onClick={
+                        ()=>
+                        dispatch({
+                            type:'LOG_IN',
+                            payload : { name :'Nahid' }
+                        })
+                    }>log in</button>
+                    <div>{JSON.stringify(state)}</div>
                 </div>
             </Layout>
         </div>
